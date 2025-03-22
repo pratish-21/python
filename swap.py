882b35f29e84c2fcb54008da487cf048affa26ae
pratish-21 <jangampratish1@gmail.com>
2025-03-22 05:33:11 +0530

    swap

---
+++ swap.py
@@ -0,0 +1,9 @@
+a=input("enter value of a: ")
+b=input("enter value of b: ")
+print("value of a before swapping : "+a)
+print("value of b before swapping : "+b)
+c=a
+a=b
+b=c
+print("value of a after swapping : " + a)
+print("value of b after swapping : " + b)
