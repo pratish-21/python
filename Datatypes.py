985f8fd625aa2c1dd466523e2700f8258c3ab17b
pratish-21 <jangampratish1@gmail.com>
2025-03-25 05:46:51 +0530

    datatypes

---
+++ Datatypes.py
@@ -0,0 +1,38 @@
+var = 12345678905765
+print(var)
+var2 = 10.1
+print(var+var2)
+print(type(var+var2))
+
+
+a = 0B10
+b = 0O10
+c = 0X10
+
+print(a, b ,c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+y="pratish"
+z=2
+print(y)
+print(type(y))
+print(y[6])
+name1="123"
+name2="1"
+print(name1+name2)
+#print(y+z)
+print("Pratish\'s \\n \"Lectures\""*5)
+print("\\n")
+
+var = True
+
+print(var)
+print(type(var))
+
+a=11
+b=2
+var=a<b
+print(var)
+print(type(var))
