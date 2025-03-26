d5f627f3b74afd5c8e71c8a304a9e419a1fd827c
pratish-21 <jangampratish1@gmail.com>
2025-03-26 05:34:47 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,24 @@
+length = len("Pratish")
+print("Hi , Your name has"+" "+str(length)+" "+"characters")
+print(type(length))
+
+"""
+int()
+float()
+str()
+tyoe casting
+"""
+
+print(10+10)
+
+print("10"+"10")
+print(int("10")+int("10"))
+print(10+float("10"))
+
+#print(10+int("pratish"))
+
+a = input("enter first number: ")
+b = input("enter second number: ")
+c=int(a)+int(b)
+print(c)
+
