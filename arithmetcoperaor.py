9b2d1192d98701853ffc55f5a89fa9e899f6372a
pratish-21 <jangampratish1@gmail.com>
2025-03-29 05:39:11 +0530

    arithmetic operator

---
+++ arithmetcoperaor.py
@@ -0,0 +1,29 @@
+""""
+print(5+2)
+print(5-2)
+print(5*2)
+print(4/2)
+print(3//7)
+print(2%4)
+print(5**2)
+
+a=5+2%4-3//7*24**2/2
+b=4-2
+c=int(7*8)
+print(b*c)
+
+a=5+2*(3-1)+10/5
+print(a)
+
+
+a=float(input("enter body weight in KG : "))
+b=float(input("enter height in meter : "))
+c=a/b**2
+print("BMI Value of the person is : "+str(c))
+"""
+
+weight=int(input("enter your weight in kg :"))
+height=float(input("enter your height in metern :"))
+BMI=weight/(height*height)
+print(BMI)
+
