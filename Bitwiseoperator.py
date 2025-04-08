7f1e082580d91d0af9137bf5197e48b4fee72588
pratish-21 <jangampratish1@gmail.com>
2025-04-08 05:56:53 +0530

    bitwise operator

---
+++ Bitwiseoperator.py
@@ -0,0 +1,15 @@
+#&,|,^,<<,>>
+a,b=5,4
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<2)
+print(a>>2)
+print()
+print(26&23)
+print(17|24)
+print(17^24)
+print(~45)
+print(68<<2)
+print(56>>3)
