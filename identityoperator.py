1cea57f2d83f82d5b68c28fea15ab62288a2bc24
pratish-21 <jangampratish1@gmail.com>
2025-04-09 05:30:13 +0530

    identity operator

---
+++ identityoperator.py
@@ -0,0 +1,38 @@
+a=5
+b=5
+print(id(a))
+print(id(b))
+print(a is b)
+print(a is not b)
+print("\n")
+m=5
+n='5'
+print(id(m))
+print(id(n))
+print(m is n)
+print(m is not n)
+print("\n")
+
+p=4
+print(id(p))
+p=5
+print(id(p))
+print(p is p)
+print()
+
+a=5
+print(id(a))
+a=8
+print(id(a))
+print(a is a)
+
+print()
+print()
+
+
+c=3
+d=3
+print(id(c))
+print(id(d))
+print(id(3))
+print(c is d is 3)
