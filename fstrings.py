38d1019b56df74c0032fa9bbf5ff8035f0610924
pratish-21 <jangampratish1@gmail.com>
2025-08-05 22:38:13 +0530

    fstrings

---
+++ fstrings.py
@@ -0,0 +1,7 @@
+name = "pratish"
+age = 30
+height = 1.6
+print("My name is" + " " +name+" "+"my age is" +" "+str(age)+" ""my height is"+" "+str(height))
+print("My name is" ,name,"my age is" ,age,"my height is",height)
+print(f"My name is {name} my age is {age} my height is {height}")
+print(f"My name is {name} my age is {age*2} my height is {height}")
